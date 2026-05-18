c5850148f62ac5c4a24e7f7bc5bd357e6a6ef053
0800 <spmspy0800@gmail.com>
2026-05-18 18:28:01 +0200

    update app.js

--- app.js
+++ app.js
@@ -13,20 +13,11 @@ if (currentGame) {
   }
 
   document.body.innerHTML = `
-    <iframe
-      src="games/${currentGame}.html"
-      style="
-        position: fixed;
-        inset: 0;
-        width: 100%;
-        height: 100%;
-        border: none;
-      "
-      allowfullscreen
-    ></iframe>
+      <a href="/" style="color: white;">← Back to Homepage</a><h3 style="text-align: center;">PGIS</h3>
+        <iframe src="games/${currentGame}.html" style="height: 638px; width: 100%; max-width: 1500px; border: none; display: block; margin: 20px auto;"  title="${game}"></iframe>
   `;
 } else {
   gamesEl.innerHTML = games.map(game => `
-    <div class="gamediv"><b>${game}</b><img src="" alt="game image" width="100px" height="100px"><a href="?game=${game}"><button>play</button></a> <button class="favorite-btn">☆</button></div>
+    <div class="gamediv"><b>${game}</b><img src="" alt="game image" width="100px" height="100px"><a href="?game=${game}"><button>play</button></a></div>
   `).join("");
 }
