2bb99bff1ec18326a0476167af424588be6b9d78
0800 <spmspy0800@gmail.com>
2026-05-18 17:25:52 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const gamesEl = document.body;
+
+const games = await fetch("games.json")
+  .then(r => r.json());
+
+const params = new URLSearchParams(location.search);
+const currentGame = params.get("game");
+
+if (currentGame) {
+  if (!games.includes(currentGame)) {
+    document.body.innerHTML = "Game not found";
+    throw new Error("Game not found");
+  }
+
+  gamesEl.innerHTML = `
+    <iframe
+      src="games/${currentGame}.html"
+      style="
+        position: fixed;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+        border: none;
+      "
+      allowfullscreen
+    ></iframe>
+  `;
+} else {
+  gamesEl.innerHTML = games.map(game => `
+    <a
+      href="?game=${game}"
+      style="
+        display:block;
+        margin:10px;
+        font-size:24px;
+      "
+    >
+      ${game}
+    </a>
+  `).join("");
+}
