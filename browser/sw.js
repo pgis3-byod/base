53214b7b3e6ba9c7b5fb6bec92cd55913ce2396b
0800 <spmspy0800@gmail.com>
2026-05-29 10:26:36 +0200

    Update sw.js

--- browser/sw.js
+++ browser/sw.js
@@ -6,15 +6,6 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim())
 })
 
-// Only handle Site A scope requests
 self.addEventListener("fetch", (event) => {
-    const url = new URL(event.request.url)
-
-    // If request is NOT Site A, just fetch normally
-    if (url.origin !== location.origin) {
-        event.respondWith(fetch(event.request))
-        return
-    }
-
     event.respondWith(fetch(event.request))
 })
