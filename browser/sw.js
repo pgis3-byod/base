4a1a16b6cab139cf207f275e7416793700527bb0
0800 <spmspy0800@gmail.com>
2026-05-29 10:49:53 +0200

    Update sw.js

--- browser/sw.js
+++ browser/sw.js
@@ -1,11 +1,16 @@
-self.addEventListener("install", () => {
-    self.skipWaiting()
-})
+importScripts("scram/scramjet.all.js")
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil(self.clients.claim())
-})
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+
+async function handleRequest(event) {
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+    return fetch(event.request);
+}
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request))
-})
+    event.respondWith(handleRequest(event));
+});
